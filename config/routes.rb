84b6d5e9019326c70dda7a3140c6b5390cec0afe
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-23 15:39:28 +0000

    finished implementation of delete method

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   patch "tasks/:id", to: "tasks#update"
   # Delete a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
