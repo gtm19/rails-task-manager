e5ef2f318ae7f323159ca533aeef98a1709f19b8
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-23 13:05:32 +0000

    initialise find view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Show all tasks
   get "tasks", to: "tasks#index"
   # Show a single task
+  get "tasks/:id", to: "tasks#find"
   # Create a task
   # Edit a task
   # Delete a task
