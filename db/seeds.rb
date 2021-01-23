6e2bcac10bd2fc4f3b5baf848272092d0258b447
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-23 12:15:03 +0000

    added db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  task = Task.new(
+    title: "#{Faker::Verb.base} #{Faker::Hacker.noun}",
+    details: Faker::Hacker.say_something_smart,
+    completed: [true, false].sample
+  )
+  task.save
+end
