8c4364a304ee5058cc86278d0afc8c88c8941262
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-23 15:44:18 +0000

    removed all my good work from routes and replaced with 'resources :tasks'

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do  
-  # Show all tasks
-  get "tasks", to: "tasks#index"
-  # Create a task
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
-  # Show a single task
-  get "tasks/:id", to: "tasks#show", as: :task
-  # Edit a task
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
-  # Delete a task
-  delete "tasks/:id", to: "tasks#destroy"
+  # # Show all tasks
+  # get "tasks", to: "tasks#index"
+  # # Create a task
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+  # # Show a single task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # # Edit a task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # # Delete a task
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
