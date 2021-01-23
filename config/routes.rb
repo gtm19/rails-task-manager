d215a73f0c26449f006f0b7d56171c67928cc3fe
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-23 12:33:36 +0000

    added font awesome, started process of adding index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do  
+  # Show all tasks
+  get "tasks", to: "tasks#index"
+  # Show a single task
+  # Create a task
+  # Edit a task
+  # Delete a task
 end
