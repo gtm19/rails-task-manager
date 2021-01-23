f7374e61d78702bceedb8f0f82f50b7f5416b7fc
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-23 14:54:14 +0000

    complete create implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do  
   # Show all tasks
   get "tasks", to: "tasks#index"
+  # Create a task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
   # Show a single task
   get "tasks/:id", to: "tasks#show", as: :task
-  # Create a task
   # Edit a task
   # Delete a task
 end
