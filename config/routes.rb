cfb98c39c20b655f7363e8f5adb15eb605b06c69
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-23 15:23:34 +0000

    complete edit methods

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # Show a single task
   get "tasks/:id", to: "tasks#show", as: :task
   # Edit a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
   # Delete a task
 end
